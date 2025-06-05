69fae4963e5de7dd2b31d21a0b4eaa029830bea9
NicoAgnello <nikoagnello1@gmail.com>
2025-06-05 16:39:09 -0300

    avance front

--- public/main.js
+++ public/main.js
@@ -1,5 +1,43 @@
 // public/main.js
-const socket = io();
+// const socket = io();
 
-console.log('Conectado con Socket.IO');
-console.log(socket)
+// console.log('Conectado con Socket.IO');
+// console.log(socket)
+
+
+
+// Funcion Modal
+window.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal');
+  const btnAceptar = document.getElementById('aceptar-btn');
+  const inputAlias = document.getElementById('alias-input');
+  const errorMsg = document.getElementById('error-msg');
+    
+  const aliasGuardado = localStorage.getItem('alias');
+  let bienvenido = document.querySelector('.saludo')
+
+  if (aliasGuardado) {
+    modal.style.display = 'none';
+    console.log('Alias ya guardado:', aliasGuardado);
+  }
+
+  btnAceptar.addEventListener('click', () => {
+    const alias = inputAlias.value.trim();
+    if (alias) {
+      localStorage.setItem('alias', alias);
+      modal.style.display = 'none';
+      console.log('Alias ingresado:', alias);
+    } else {
+      errorMsg.style.display = 'block';
+    }
+  });
+
+  inputAlias.addEventListener('input', () => {
+    if (inputAlias.value.trim() !== '') {
+      errorMsg.style.display = 'none';
+    }
+  });
+
+  bienvenido.textContent = `Bienvenido ${aliasGuardado}`;
+  
+});
