07d02ffa126f10790d729ab8c116844152f76f42
NicoAgnello <nikoagnello1@gmail.com>
2025-06-06 16:37:46 -0300

    fix bienvenido

--- public/main.js
+++ public/main.js
@@ -4,8 +4,6 @@
 // console.log('Conectado con Socket.IO');
 // console.log(socket)
 
-
-
 // Funcion Modal
 window.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('modal');
@@ -19,6 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
   if (aliasGuardado) {
     modal.style.display = 'none';
     console.log('Alias ya guardado:', aliasGuardado);
+    bienvenido.textContent = `Bienvenido ${aliasGuardado}`;
   }
 
   btnAceptar.addEventListener('click', () => {
@@ -27,6 +26,8 @@ window.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('alias', alias);
       modal.style.display = 'none';
       console.log('Alias ingresado:', alias);
+      bienvenido.textContent = `Bienvenido ${alias}`;
+      
     } else {
       errorMsg.style.display = 'block';
     }
@@ -37,7 +38,6 @@ window.addEventListener('DOMContentLoaded', () => {
       errorMsg.style.display = 'none';
     }
   });
-
-  bienvenido.textContent = `Bienvenido ${aliasGuardado}`;
   
+
 });
